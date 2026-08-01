3fa2e66c11072f9b5dadd5e0631ea47e532c7ed2
Anshul Singh <146714239+zero1cee@users.noreply.github.com>
2026-08-01 16:11:11 +0530

    completed loops

---
+++ ml/miniproj3.py
@@ -0,0 +1,12 @@
+gold = {
+    "Open":3340,
+    "High":3365,
+    "Low":3335,
+    "Close":3355
+}
+
+
+print("Todays Gold Data : ")
+
+for key , value in gold.items():
+    print(key , " : ", value)
